d2474d5f988402d54cbeff1f3a733d7b075593fd
jhonatan Jahir Cabos Angulo <jjcabosangulo.dev@gmail.com>
2022-11-21 13:11:21 -0500

    fixed some issue

--- packages/FME/voyager/src/Http/Controllers/Bread/CategoryBreadController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace TCG\Voyager\Http\Controllers\Bread;
-
-use TCG\Voyager\Http\Controllers\VoyagerBaseController;
-
-class CategoryBreadController extends VoyagerBaseController
-{
-}
